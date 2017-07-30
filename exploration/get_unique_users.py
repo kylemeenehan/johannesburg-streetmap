a5df3b13863c13e7282fa9f7fcde2d169b79dff1
Kyle Meenehan <kylemeenehan@gmail.com>
2017-07-30 20:44:05 +0200

    added function to get unique users, refactored main.py to call an explore function

---
+++ exploration/get_unique_users.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xml.etree.cElementTree as ET
+import pprint
+import re
+"""
+Your task is to explore the data a bit more.
+The first task is a fun one - find out how many unique users
+have contributed to the map in this particular area!
+
+The function process_map should return a set of unique user IDs ("uid")
+"""
+
+def get_user(element):
+    return element.attrib['uid']
+
+
+def get_unique_users(filename):
+    users = set()
+    for _, element in ET.iterparse(filename):
+        if 'uid' in element.attrib:
+            user = get_user(element)
+            if user not in users:
+                users.add(user)
+
+    return users
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import exploration.count_tags as CT
+import exploration.get_unique_users as uu
 import output.pprint as pp
 
 johannesburg = 'data/johannesburg.osm'
 
-tag_count = CT.count_tags(johannesburg)
-pp.print_dict(tag_count)
+
+def explore():
+    print 'Tag Count'
+    tag_count = CT.count_tags(johannesburg)
+    pp.print_dict(tag_count)
+    print len(uu.get_unique_users(johannesburg))
+
+
+if __name__ == '__main__':
+    explore()
