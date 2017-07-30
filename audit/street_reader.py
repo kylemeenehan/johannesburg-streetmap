210a3d5be86acd4d81d17868ee485dca93c5acef
Kyle Meenehan <kylemeenehan@gmail.com>
2017-07-30 23:03:23 +0200

    made street reader class so that processes can run line by line as opposed to running the whole document over and over

---
+++ audit/street_reader.py
@@ -0,0 +1,27 @@
+import xml.etree.cElementTree as ET
+from collections import defaultdict
+import re
+
+class Street_Reader:
+
+    street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+    def __init__(self):
+        self.street_types = []
+
+    def is_street_name(self,elem):
+        return (elem.attrib['k'] == "addr:street")
+
+    def extract_street_type(self,street_name):
+        m = self.street_type_re.search(street_name)
+        if m:
+            return m.group()
+
+
+    def get_street_types(self, elem):
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if self.is_street_name(tag):
+                    street_type = self.extract_street_type(tag.attrib['v'])
+                    if street_type not in self.street_types:
+                        self.street_types.append(street_type)
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+import xml.etree.cElementTree as ET
 import exploration.count_tags as CT
 import exploration.get_unique_users as uu
-import audit.get_street_types as gst
+import audit.street_reader as sr
 import output.pprint as pp
 
 johannesburg = 'data/johannesburg.osm'
 
 
 def explore():
-    print 'Tag Count'
-    tag_count = CT.count_tags(johannesburg)
-    pp.print_dict(tag_count)
-    print len(uu.get_unique_users(johannesburg))
-    print "street types :"
-    gst.get_street_types(johannesburg)
+    # print 'Tag Count'
+    # tag_count = CT.count_tags(johannesburg)
+    # pp.print_dict(tag_count)
+    # print len(uu.get_unique_users(johannesburg))
+    street_reader = sr.Street_Reader()
+    for event, elem in ET.iterparse(johannesburg):
+        street_reader.get_street_types(elem)
+    for street_type in street_reader.street_types:
+        print street_type
 
 
 if __name__ == '__main__':
