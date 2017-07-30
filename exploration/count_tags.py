11f96e4fdf8887bf82cbb8cd8704ccc58409736f
Kyle Meenehan <kylemeenehan@gmail.com>
2017-07-30 20:00:28 +0200

    started project structure with a function that counts that number of tags

---
+++ exploration/count_tags.py
@@ -0,0 +1,10 @@
+import xml.etree.cElementTree as ET
+
+def count_tags(file):
+    tags = {}
+    for event, elem in ET.iterparse(file):
+        if elem.tag in tags:
+            tags[elem.tag] += 1
+        else:
+            tags[elem.tag] = 1
+    print tags
---
+++ main.py
@@ -0,0 +1,5 @@
+import exploration.count_tags as CT
+
+johannesburg = 'data/johannesburg.osm'
+
+CT.count_tags(johannesburg)
