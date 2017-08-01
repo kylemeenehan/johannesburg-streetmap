333c1679366767cf49f893ee4613ed2df1ddf2c1
Kyle Meenehan <kylemeenehan@gmail.com>
2017-08-01 15:23:52 +0200

    added comments to street reader

--- audit/street_reader.py
+++ audit/street_reader.py
@@ -3,6 +3,7 @@ import re
 
 class Street_Reader:
 
+    # RegEx to get the last word in a series of words under the assumption that the last word is the street type.
     street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 
     def __init__(self):
@@ -10,15 +11,20 @@ class Street_Reader:
         self.street_types = []
         self.unexpected_streets = []
 
+    # Checks whether the element passed in contains a street name
     def is_street_name(self,elem):
         return (elem.attrib['k'] == "addr:street")
 
+    # Uses a regular expression to get the last word in a street. The assumption is that this is the street type.
     def extract_street_type(self,street_name):
         m = self.street_type_re.search(street_name)
         if m:
             return m.group()
 
-
+    # Reads in an element and determines whether it's a node or a way tag
+    # If it is a node or a way tag, iterates through tags and determines whether one of them is a street name
+    # If it is a street name, appends the street type to the list of street names, only if it isn't there already
+    # If it is not an expected street name, adds the full street name to the unexpected_streets
     def read_elem(self, elem):
         if elem.tag == "node" or elem.tag == "way":
             for tag in elem.iter("tag"):
@@ -28,7 +34,8 @@ class Street_Reader:
                         self.street_types.append(street_type)
                     if street_type not in self.expected_street_types:
                         self.unexpected_streets.append(tag.attrib['v'])
-
+    
+    # Prints all of the street types found
     def print_street_types(self):
         print "Here is a list of all the street types: \n"
         self.street_types.sort()
@@ -36,6 +43,7 @@ class Street_Reader:
             print street_type
         print
     
+    # Print the full name of streets that have an unexpected street name
     def print_unexpected_streets(self):
         print "Here is a list of streets with unexpected street names: \n"
         for street in self.unexpected_streets:
