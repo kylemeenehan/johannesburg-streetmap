96882933ec74a0abbbbb19fe0e75e59c51733641
Kyle Meenehan <kylemeenehan@gmail.com>
2017-08-16 10:37:04 +0200

    added schema and code from example code to write to csv

---
+++ output/write_to_csv.py
@@ -0,0 +1,167 @@
+import csv
+import codecs
+import pprint
+import re
+import xml.etree.cElementTree as ET
+
+import cerberus
+
+import schema
+
+OSM_PATH = "../data/johannesburg.osm"
+
+NODES_PATH = "nodes.csv"
+NODE_TAGS_PATH = "nodes_tags.csv"
+WAYS_PATH = "ways.csv"
+WAY_NODES_PATH = "ways_nodes.csv"
+WAY_TAGS_PATH = "ways_tags.csv"
+
+LOWER_COLON = re.compile(r'^([a-z]|_)+:([a-z]|_)+')
+PROBLEMCHARS = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+SCHEMA = schema.schema
+
+# Make sure the fields order in the csvs matches the column order in the sql table schema
+NODE_FIELDS = ['id', 'lat', 'lon', 'user',
+               'uid', 'version', 'changeset', 'timestamp']
+NODE_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_FIELDS = ['id', 'user', 'uid', 'version', 'changeset', 'timestamp']
+WAY_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_NODES_FIELDS = ['id', 'node_id', 'position']
+
+
+def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIELDS,
+                  problem_chars=PROBLEMCHARS, default_tag_type='regular'):
+    """Clean and shape node or way XML element to Python dict"""
+
+    node_attribs = {}
+    way_attribs = {}
+    way_nodes = []
+    tags = []  # Handle secondary tags the same way for both node and way elements
+
+    # YOUR CODE HERE
+    if element.tag == 'node':
+        for field in NODE_FIELDS:
+            node_attribs[field] = element.attrib[field]
+        for tag in element.iter('tag'):
+            new_tag = {}
+            tk = get_type_and_key(tag)
+            new_tag["id"] = element.attrib['id']
+            new_tag["key"] = tk['key']
+            new_tag["value"] = tag.attrib['v']
+            new_tag["type"] = tk['type']
+            tags.append(new_tag)
+        return {'node': node_attribs, 'node_tags': tags}
+    elif element.tag == 'way':
+        for field in WAY_FIELDS:
+            way_attribs[field] = element.attrib[field]
+        for position, nd in enumerate(element.iter('nd')):
+            way_node = {}
+            way_node["id"] = element.attrib['id']
+            way_node["node_id"] = nd.attrib['ref']
+            way_node["position"] = position
+            way_nodes.append(way_node)
+
+        for tag in element.iter('tag'):
+            new_tag = {}
+            tk = get_type_and_key(tag)
+            new_tag["id"] = element.attrib['id']
+            new_tag["key"] = tk['key']
+            new_tag["value"] = tag.attrib['v']
+            new_tag["type"] = tk['type']
+            tags.append(new_tag)
+
+        return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
+
+
+# ================================================== #
+#               Helper Functions                     #
+# ================================================== #
+
+def get_type_and_key(tag):
+    s = tag.attrib['k'].split(':', 1)
+    if len(s) > 1:
+        return {'key': s[1], 'type': s[0]}
+    else:
+        return {'key': s[0], 'type': 'regular'}
+
+
+def get_element(osm_file, tags=('node', 'way', 'relation')):
+    """Yield element if it is the right type of tag"""
+
+    context = ET.iterparse(osm_file, events=('start', 'end'))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
+
+
+def validate_element(element, validator, schema=SCHEMA):
+    """Raise ValidationError if element does not match schema"""
+    if validator.validate(element, schema) is not True:
+        field, errors = next(validator.errors.iteritems())
+        message_string = "\nElement of type '{0}' has the following errors:\n{1}"
+        error_string = pprint.pformat(errors)
+
+        raise Exception(message_string.format(field, error_string))
+
+
+class UnicodeDictWriter(csv.DictWriter, object):
+    """Extend csv.DictWriter to handle Unicode input"""
+
+    def writerow(self, row):
+        super(UnicodeDictWriter, self).writerow({
+            k: (v.encode('utf-8') if isinstance(v, unicode) else v) for k, v in row.iteritems()
+        })
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+# ================================================== #
+#               Main Function                        #
+# ================================================== #
+def process_map(file_in, validate):
+    """Iteratively process each XML element and write to csv(s)"""
+
+    with codecs.open(NODES_PATH, 'w') as nodes_file, \
+            codecs.open(NODE_TAGS_PATH, 'w') as nodes_tags_file, \
+            codecs.open(WAYS_PATH, 'w') as ways_file, \
+            codecs.open(WAY_NODES_PATH, 'w') as way_nodes_file, \
+            codecs.open(WAY_TAGS_PATH, 'w') as way_tags_file:
+
+        nodes_writer = UnicodeDictWriter(nodes_file, NODE_FIELDS)
+        node_tags_writer = UnicodeDictWriter(nodes_tags_file, NODE_TAGS_FIELDS)
+        ways_writer = UnicodeDictWriter(ways_file, WAY_FIELDS)
+        way_nodes_writer = UnicodeDictWriter(way_nodes_file, WAY_NODES_FIELDS)
+        way_tags_writer = UnicodeDictWriter(way_tags_file, WAY_TAGS_FIELDS)
+
+        nodes_writer.writeheader()
+        node_tags_writer.writeheader()
+        ways_writer.writeheader()
+        way_nodes_writer.writeheader()
+        way_tags_writer.writeheader()
+
+        validator = cerberus.Validator()
+
+        for element in get_element(file_in, tags=('node', 'way')):
+            el = shape_element(element)
+            if el:
+                if validate is True:
+                    validate_element(el, validator)
+
+                if element.tag == 'node':
+                    nodes_writer.writerow(el['node'])
+                    node_tags_writer.writerows(el['node_tags'])
+                elif element.tag == 'way':
+                    ways_writer.writerow(el['way'])
+                    way_nodes_writer.writerows(el['way_nodes'])
+                    way_tags_writer.writerows(el['way_tags'])
+
+
+if __name__ == '__main__':
+    # Note: Validation is ~ 10X slower. For the project consider using a small
+    # sample of the map when validating.
+    process_map(OSM_PATH, validate=False)
